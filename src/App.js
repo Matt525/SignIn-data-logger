0d252efe96ece99882dc2c55246bcf386e3b24c1
MATTHEW STATON <43524360+Matt525@users.noreply.github.com>
2020-07-28 12:25:06 -0400

    Removed JS Removed Console.time()

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ export default function App() {
 
   return (
     <React.Fragment>
-      {console.time()}
+      
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Form} />
           <Route path="/info" component={Info} />
         </Switch>
       </BrowserRouter>
-      {console.timeEnd()}
+      
     </React.Fragment>
   );
 }
