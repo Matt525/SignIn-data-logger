d0444f7d445e2e49178eaa023dcc372a6bde00b2
Matthew Statongit config --global user.name Matthew <Malanstaton@gmail.com>
2020-08-05 17:38:08 -0400

    route fix attempt

--- src/Form.js
+++ src/Form.js
@@ -56,7 +56,7 @@ export default class Form extends React.Component{
                          
                             
 
-            $.post('/sendMail',transferData,()=>{console.log("POST Received")})
+            $.post('http://immense-brook-68010.herokuapp.com/sendMail',transferData,()=>{console.log("POST Received")})
       }
         
                             handleClear = ()=>{
