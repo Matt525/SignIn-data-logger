a1d84dcbecbcc5850857a85a2753fae76d65da8c
Matthew Statongit config --global user.name Matthew <Malanstaton@gmail.com>
2020-08-05 21:00:19 -0400

    transporter syntax update

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post('/sendMail',(req,res)=>{
         service: 'Gmail',
         host: 'smtp.gmail.com',
         port: 587,
-        secure: false
+        secure: false,
         auth: { 
             user: process.env.EMAIL,
             pass: process.env.PASS
